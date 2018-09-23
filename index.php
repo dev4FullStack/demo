b85c18ec735863f06349b6ac60d189df5281f332
Harold Payet <webpostaction@gmail.com>
2018-09-23 07:21:09 +0200

    Ajout des fichiers html

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>My Buisness</title>
+	<link rel="stylesheet" type="text/css" href="style.css" >
+</head>
+<body>
+<div class="calque">
+	
+
+	<div class="companyname">
+		<h3>companyname</h3>
+	</div>
+	
+	<nav class="menu">
+		<ul id="menuStyle">
+			<li><a href="#home">home</a></li>
+			<li><a href="#about">about</a></li>
+			<li><a href="#portfolio">portfolio</a></li>
+			<li><a href="#contact">contact</a></li>
+		</ul>
+	</nav>
+	
+	<div class="infoTitle">
+		<h1>Were're here to create your<br />online presence and style</h1>
+	</div>
+
+	<div class="infoLorem">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, impedit, delectus ex ipsa velit nesciunt corporis consequuntur deleniti, quidem temporibus dicta facere doloribus quam!</p>
+	</div>
+	<button class="startToday">start today with us</button>
+</div>
+	<div class="features">
+		<h1>Features you'll loves</h1>
+		
+		<div class="featuresContent">
+			<button class="btn-human"></button>
+			<h2>PERSONNAL TOUCH</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet molestias ab rerum aliquid quia, ad eos. Facere architecto reprehenderit saepe aliquid, quaerat vero ducimus.</p>
+		</div>
+
+		<div class="featuresContent2">
+			<button class="btn-pen"></button>
+			<h2>UNIQUE SOLUTIONS</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet molestias ab rerum aliquid quia, ad eos. Facere architecto reprehenderit saepe aliquid, quaerat vero ducimus.</p>
+		</div>
+
+		<div class="featuresContent3">
+			<button class="btn-flag"></button>
+			<h2>HELP IN PROMOTION</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet molestias ab rerum aliquid quia, ad eos. Facere architecto reprehenderit saepe aliquid, quaerat vero ducimus.</p>
+		</div>
+
+
+	
+	</div>
+
+	<div class="story">
+		<div class="emptyStory">
+		
+		</div>
+		<div class="shortStory">
+			<h1>Our short story</h1>
+			<div class="storyGo">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum deserunt ad nihil rem ea quisquam? Ad consequuntur suscipit alias sunt voluptatibus, vitae mollitia minus harum? Blanditiis reprehenderit est voluptas, aut
+			</p>
+			<br />
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam facere sunt natus unde sint. Velit provident recusandae, accusamus eos aspernatur ipsum. Veniam non, neque, veritatis laborum aliquid animi. Veniam reiciendis labore quisquam libero similique doloribus sapiente delectus maxime veritatis minima?
+			</p>
+			<button class="storyClick">more about our team</button>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="clientsSays">
+		<h1>our clients says</h1>
+		<div class="left">
+			<div class="clientsMsg">
+				<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima mollitia esse labore, ex velit aut 	hic inventore quisquam veniam ullam porro, deleniti blanditiis ducimus dolor doloribus excepturi, tempore nobis rem eveniet debitis nam laudantium vel. Doloribus officia, nemo nisi facilis architecto suscipit, ad in alias autem esse voluptatum beatae, repellendus eligendi numquam quod rerum. Est, officia!
+				</p>
+				<div class="joeDoe"><h3>Joe Doe</h3><p>Creative Deirector Gmoogle.com</p><div class="joeScreen"></div></div>
+			</div>
+
+			<div class="clientsMsg">
+				<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima mollitia esse labore, ex velit aut 	hic inventore quisquam veniam ullam porro, deleniti blanditiis ducimus dolor doloribus excepturi, tempore nobis rem eveniet debitis nam laudantium vel. Doloribus officia, nemo nisi facilis architecto suscipit, ad in alias autem esse voluptatum beatae, repellendus eligendi numquam quod rerum. Est, officia!
+				</p>
+				<div class="joeDoe"><h3>Joe Doe</h3><p>Creative Deirector Gmoogle.com</p><div class="joeScreen"></div></div>
+			</div>
+
+		</div>
+		
+		<div class="right">
+			
+
+		</div>
+
+	</div>	
+
+</body>
+</html>
